8667e435b3644e01d976a3b63aa2a03dd22869e3
rex2099 <150912315+rex2099@users.noreply.github.com>
2026-08-31 19:57:25 +0800

    Add native menu bar status and controls
    Updated version number to 0.2.0 and added new properties for automation control in LockPowerWatcher.

--- Sources/LockPower/main.m
+++ Sources/LockPower/main.m
@@ -2,8 +2,16 @@
 #import <CoreGraphics/CoreGraphics.h>
 #import <Foundation/Foundation.h>
 
-static const char *kVersion = "0.1.0";
+static const char *kVersion = "0.2.0";
 static const NSTimeInterval kDefaultDelaySeconds = 120.0;
+
+typedef NS_ENUM(NSInteger, LockPowerStatusState) {
+    LockPowerStatusStateFullPerformance,
+    LockPowerStatusStatePending,
+    LockPowerStatusStateLowPower,
+    LockPowerStatusStatePaused,
+    LockPowerStatusStateError,
+};
 
 static void LogMessage(NSString *format, ...) {
     va_list arguments;
@@ -44,11 +52,21 @@ static BOOL CurrentSessionIsLocked(void) {
     return [session[@"CGSSessionScreenIsLocked"] boolValue];
 }
 
-@interface LockPowerWatcher : NSObject
+@interface LockPowerWatcher : NSObject <NSMenuDelegate>
 @property(nonatomic) NSTimeInterval delay;
 @property(nonatomic) BOOL screenLocked;
 @property(nonatomic) BOOL displayAsleep;
 @property(nonatomic) BOOL enablePending;
+@property(nonatomic) BOOL automationPaused;
+@property(nonatomic) LockPowerStatusState statusState;
+@property(nonatomic, strong) NSDate *scheduledEnableDate;
+@property(nonatomic, copy) NSString *lastEvent;
+@property(nonatomic, strong) NSStatusItem *statusItem;
+@property(nonatomic, strong) NSMenuItem *statusMenuItem;
+@property(nonatomic, strong) NSMenuItem *detailMenuItem;
+@property(nonatomic, strong) NSMenuItem *pauseMenuItem;
+@property(nonatomic, strong) NSMenuItem *enableMenuItem;
+@property(nonatomic, strong) NSMenuItem *disableMenuItem;
 @end
 
 @implementation LockPowerWatcher
@@ -63,6 +81,9 @@ static BOOL CurrentSessionIsLocked(void) {
     self.screenLocked = CurrentSessionIsLocked();
     self.displayAsleep = NO;
     self.enablePending = NO;
+    self.automationPaused = NO;
+    self.statusState = LockPowerStatusStateFullPerformance;
+    [self setUpStatusItem];
 
     NSNotificationCenter *workspaceCenter = NSWorkspace.sharedWorkspace.notificationCenter;
     [workspaceCenter addObserver:self
@@ -103,6 +124,197 @@ static BOOL CurrentSessionIsLocked(void) {
     [NSObject cancelPreviousPerformRequestsWithTarget:self];
     [NSWorkspace.sharedWorkspace.notificationCenter removeObserver:self];
     [NSDistributedNotificationCenter.defaultCenter removeObserver:self];
+}
+
+- (void)setUpStatusItem {
+    self.statusItem = [NSStatusBar.systemStatusBar
+                       statusItemWithLength:NSSquareStatusItemLength];
+
+    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"LockPower"];
+    menu.delegate = self;
+
+    self.statusMenuItem = [[NSMenuItem alloc] initWithTitle:@"LockPower"
+                                                     action:nil
+                                              keyEquivalent:@""];
+    self.statusMenuItem.enabled = NO;
+    [menu addItem:self.statusMenuItem];
+
+    self.detailMenuItem = [[NSMenuItem alloc] initWithTitle:@""
+                                                     action:nil
+                                              keyEquivalent:@""];
+    self.detailMenuItem.enabled = NO;
+    [menu addItem:self.detailMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+
+    self.enableMenuItem = [[NSMenuItem alloc]
+                           initWithTitle:@"Enable Low Power Mode Now"
+                                  action:@selector(enableLowPowerNow:)
+                           keyEquivalent:@""];
+    self.enableMenuItem.target = self;
+    [menu addItem:self.enableMenuItem];
+
+    self.disableMenuItem = [[NSMenuItem alloc]
+                            initWithTitle:@"Restore Full Performance Now"
+                                   action:@selector(restoreFullPerformanceNow:)
+                            keyEquivalent:@""];
+    self.disableMenuItem.target = self;
+    [menu addItem:self.disableMenuItem];
+    [menu addItem:NSMenuItem.separatorItem];
+
+    self.pauseMenuItem = [[NSMenuItem alloc] initWithTitle:@"Pause Automation"
+                                                    action:@selector(toggleAutomationPaused:)
+                                             keyEquivalent:@""];
+    self.pauseMenuItem.target = self;
+    [menu addItem:self.pauseMenuItem];
+
+    NSMenuItem *showLogItem = [[NSMenuItem alloc] initWithTitle:@"Show Log in Finder"
+                                                         action:@selector(showLogInFinder:)
+                                                  keyEquivalent:@""];
+    showLogItem.target = self;
+    [menu addItem:showLogItem];
+
+    NSMenuItem *projectItem = [[NSMenuItem alloc] initWithTitle:@"Open LockPower on GitHub"
+                                                         action:@selector(openProjectPage:)
+                                                  keyEquivalent:@""];
+    projectItem.target = self;
+    [menu addItem:projectItem];
+
+    self.statusItem.menu = menu;
+    [self updateStatusItem];
+
+    if (self.statusItem != nil && self.statusItem.button != nil) {
+        LogMessage(@"Status item ready; symbol=%@", [self symbolName]);
+    } else {
+        LogMessage(@"Status item unavailable");
+    }
+}
+
+- (NSString *)statusTitle {
+    switch (self.statusState) {
+        case LockPowerStatusStatePending:
+            return @"Waiting to enable Low Power Mode";
+        case LockPowerStatusStateLowPower:
+            return @"Low Power Mode is on";
+        case LockPowerStatusStatePaused:
+            return @"Automation is paused";
+        case LockPowerStatusStateError:
+            return @"Power mode switch failed";
+        case LockPowerStatusStateFullPerformance:
+        default:
+            return @"Full performance mode";
+    }
+}
+
+- (NSString *)symbolName {
+    switch (self.statusState) {
+        case LockPowerStatusStatePending:
+            return @"clock";
+        case LockPowerStatusStateLowPower:
+            return @"leaf.fill";
+        case LockPowerStatusStatePaused:
+            return @"pause.fill";
+        case LockPowerStatusStateError:
+            return @"exclamationmark.triangle.fill";
+        case LockPowerStatusStateFullPerformance:
+        default:
+            return @"bolt.fill";
+    }
+}
+
+- (void)updateStatusItem {
+    NSString *title = [self statusTitle];
+    NSImage *image = [NSImage imageWithSystemSymbolName:[self symbolName]
+                               accessibilityDescription:title];
+    if (image != nil) {
+        image.template = YES;
+        self.statusItem.button.image = image;
+        self.statusItem.button.title = @"";
+    } else {
+        self.statusItem.button.image = nil;
+        self.statusItem.button.title = @"LP";
+    }
+    self.statusItem.button.toolTip = [NSString stringWithFormat:@"LockPower — %@", title];
+}
+
+- (void)setStatusState:(LockPowerStatusState)state detail:(NSString *)detail {
+    _statusState = state;
+    if (detail.length > 0) {
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.locale = NSLocale.currentLocale;
+        formatter.timeStyle = NSDateFormatterMediumStyle;
+        formatter.dateStyle = NSDateFormatterNoStyle;
+        self.lastEvent = [NSString stringWithFormat:@"%@ · %@",
+                          detail,
+                          [formatter stringFromDate:[NSDate date]]];
+    }
+    [self updateStatusItem];
+}
+
+- (void)menuNeedsUpdate:(NSMenu *)menu {
+    (void)menu;
+    self.statusMenuItem.title = [self statusTitle];
+
+    if (self.statusState == LockPowerStatusStatePending && self.scheduledEnableDate != nil) {
+        NSInteger remaining = MAX(0, (NSInteger)ceil([self.scheduledEnableDate timeIntervalSinceNow]));
+        self.detailMenuItem.title = [NSString stringWithFormat:@"Low Power Mode in %ld seconds",
+                                     (long)remaining];
+    } else if (self.lastEvent.length > 0) {
+        self.detailMenuItem.title = self.lastEvent;
+    } else {
+        self.detailMenuItem.title = [NSString stringWithFormat:@"Delay: %.0f seconds", self.delay];
+    }
+
+    BOOL lowPowerEnabled = NSProcessInfo.processInfo.lowPowerModeEnabled;
+    self.enableMenuItem.enabled = !lowPowerEnabled && !self.automationPaused;
+    self.disableMenuItem.enabled = lowPowerEnabled;
+    self.pauseMenuItem.title = self.automationPaused ? @"Resume Automation" : @"Pause Automation";
+}
+
+- (void)cancelPendingEnable {
+    [NSObject cancelPreviousPerformRequestsWithTarget:self
+                                             selector:@selector(enableLowPowerIfStillAway)
+                                               object:nil];
+    self.enablePending = NO;
+    self.scheduledEnableDate = nil;
+}
+
+- (void)enableLowPowerNow:(id)sender {
+    (void)sender;
+    [self cancelPendingEnable];
+    [self setLowPowerMode:YES reason:@"manual menu action"];
+}
+
+- (void)restoreFullPerformanceNow:(id)sender {
+    (void)sender;
+    [self cancelPendingEnable];
+    [self setLowPowerMode:NO reason:@"manual menu action"];
+}
+
+- (void)toggleAutomationPaused:(id)sender {
+    (void)sender;
+    self.automationPaused = !self.automationPaused;
+    if (self.automationPaused) {
+        [self cancelPendingEnable];
+        LogMessage(@"Automation paused from the menu");
+        [self setLowPowerMode:NO reason:@"automation paused"];
+        [self setStatusState:LockPowerStatusStatePaused detail:@"Automation paused"];
+    } else {
+        LogMessage(@"Automation resumed from the menu");
+        [self reevaluate];
+    }
+}
+
+- (void)showLogInFinder:(id)sender {
+    (void)sender;
+    NSString *path = [@"~/Library/Logs/LockPower.log" stringByExpandingTildeInPath];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    [NSWorkspace.sharedWorkspace activateFileViewerSelectingURLs:@[url]];
+}
+
+- (void)openProjectPage:(id)sender {
+    (void)sender;
+    NSURL *url = [NSURL URLWithString:@"https://github.com/rex2099/LockPower"];
+    [NSWorkspace.sharedWorkspace openURL:url];
 }
 
 - (void)screensDidSleep:(NSNotification *)notification {
@@ -150,13 +362,24 @@ static BOOL CurrentSessionIsLocked(void) {
 }
 
 - (void)reevaluate {
+    if (self.automationPaused) {
+        [self setStatusState:LockPowerStatusStatePaused detail:nil];
+        return;
+    }
+
     BOOL away = self.screenLocked || self.displayAsleep;
     if (away) {
-        if (self.enablePending || NSProcessInfo.processInfo.lowPowerModeEnabled) {
+        if (self.enablePending) {
+            return;
+        }
+        if (NSProcessInfo.processInfo.lowPowerModeEnabled) {
+            [self setStatusState:LockPowerStatusStateLowPower detail:nil];
             return;
         }
 
         self.enablePending = YES;
+        self.scheduledEnableDate = [NSDate dateWithTimeIntervalSinceNow:self.delay];
+        [self setStatusState:LockPowerStatusStatePending detail:@"Away state detected"];
         LogMessage(@"Away state detected; Low Power Mode will turn on in %.0f seconds",
                    self.delay);
         [self performSelector:@selector(enableLowPowerIfStillAway)
@@ -165,15 +388,13 @@ static BOOL CurrentSessionIsLocked(void) {
         return;
     }
 
-    [NSObject cancelPreviousPerformRequestsWithTarget:self
-                                             selector:@selector(enableLowPowerIfStillAway)
-                                               object:nil];
-    self.enablePending = NO;
+    [self cancelPendingEnable];
     [self setLowPowerMode:NO reason:@"local session active"];
 }
 
 - (void)enableLowPowerIfStillAway {
     self.enablePending = NO;
+    self.scheduledEnableDate = nil;
     if (self.screenLocked || self.displayAsleep) {
         [self setLowPowerMode:YES reason:@"screen locked or display asleep"];
     }
@@ -182,6 +403,9 @@ static BOOL CurrentSessionIsLocked(void) {
 - (void)setLowPowerMode:(BOOL)enabled reason:(NSString *)reason {
     if (NSProcessInfo.processInfo.lowPowerModeEnabled == enabled) {
         LogMessage(@"Low Power Mode already %@ (%@)", enabled ? @"on" : @"off", reason);
+        [self setStatusState:(enabled ? LockPowerStatusStateLowPower
+                                      : LockPowerStatusStateFullPerformance)
+                      detail:reason];
         return;
     }
 
@@ -196,6 +420,7 @@ static BOOL CurrentSessionIsLocked(void) {
     NSError *launchError = nil;
     if (![task launchAndReturnError:&launchError]) {
         LogMessage(@"Failed to launch pmset: %@", launchError.localizedDescription);
+        [self setStatusState:LockPowerStatusStateError detail:@"Unable to run pmset"];
         return;
     }
 
@@ -208,10 +433,15 @@ static BOOL CurrentSessionIsLocked(void) {
 
     if (task.terminationStatus == 0) {
         LogMessage(@"Low Power Mode turned %@ (%@)", enabled ? @"on" : @"off", reason);
+        [self setStatusState:(enabled ? LockPowerStatusStateLowPower
+                                      : LockPowerStatusStateFullPerformance)
+                      detail:(enabled ? @"Low Power Mode enabled"
+                                      : @"Full performance restored")];
     } else {
         LogMessage(@"pmset failed with status %d: %@",
                    task.terminationStatus,
                    errorText.length > 0 ? errorText : @"unknown error");
+        [self setStatusState:LockPowerStatusStateError detail:@"pmset command failed"];
     }
 }
 
@@ -245,11 +475,12 @@ int main(int argc, const char *argv[]) {
             }
         }
 
-        (void)NSApplication.sharedApplication;
+        NSApplication *application = NSApplication.sharedApplication;
+        [application setActivationPolicy:NSApplicationActivationPolicyAccessory];
         LockPowerWatcher *watcher = [[LockPowerWatcher alloc]
                                      initWithDelay:ConfiguredDelay()];
         (void)watcher;
-        [NSRunLoop.currentRunLoop run];
+        [application run];
     }
     return 0;
 }
